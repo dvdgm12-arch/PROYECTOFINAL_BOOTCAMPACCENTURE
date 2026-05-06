dc9b4fd48dbfbcc9979ff97c1cf1e71052ee9089
dvdgm12-arch <dvdgm12@gmail.com>
2026-05-06 22:49:28 +0200

    FASE 0 GOLD: Añadido AppInitializer.java como sustituto de web.xml

---
+++ src/main/java/com/hotel/config/AppInitializer.java
@@ -0,0 +1,26 @@
+// SUSTITUTO DE DE WEB.XML PARA AVISAR DE ERRORES AL MOMENTO
+
+package com.hotel.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+ // CONFIGURACION DE BBDD
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { HibernateConfig.class }; 
+    }
+
+    @Override
+ // CONFIGURACION DE LA WEB
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { AppConfig.class }; 
+    }
+
+    @Override
+    // GESTION DEL SERVIDOR POR SPRING
+    protected String[] getServletMappings() {
+        return new String[] { "/" }; 
+    }
+}
