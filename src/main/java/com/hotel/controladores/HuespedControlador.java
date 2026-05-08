4d813d8cb7057c8b60524eb2853486cf07db898b
Lidia <lidiasanda@gmail.com>
2026-05-08 21:01:41 +0200

    FASE1B: AÑADO CONTROLADOR CRUD HUÉSPEDES

---
+++ src/main/java/com/hotel/controladores/HuespedControlador.java
@@ -0,0 +1,94 @@
+package com.hotel.controladores;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hotel.entidades.Huesped;
+import com.hotel.servicios.HuespedServicio;
+
+
+@Controller
+@RequestMapping("/huespedes")
+public class HuespedControlador {
+
+    @Autowired
+    private HuespedServicio huespedServicio;
+    
+    // LISTADO GENERAL
+    @GetMapping
+    public String listarHuespedes(Model modelo) {
+        List<Huesped> lista = huespedServicio.obtenerTodas();
+        modelo.addAttribute("listaHuespedes", lista);
+        
+        
+    /*  java.util.Map<String, String> usuarioFalso = new java.util.HashMap<>();
+        usuarioFalso.put("nombre", "Liante de Pruebas");
+        usuarioFalso.put("rol", "recepcionista"); 
+        modelo.addAttribute("usuarioSesion", usuarioFalso); */
+        
+        return "Huespedes";
+    }
+    
+    // DETALLE DEL HUÉSPED
+    @GetMapping("/detalle/{id}")
+    public String verDetalle(@PathVariable("id") int id, Model modelo) {
+        Huesped h = huespedServicio.obtenerPorId(id);
+        modelo.addAttribute("huesped", h);
+        
+    /*  java.util.Map<String, String> usuarioFalso = new java.util.HashMap<>();
+        usuarioFalso.put("rol", "recepcionista");
+        modelo.addAttribute("usuarioSesion", usuarioFalso); */
+        
+        return "DetalleHuesped";
+    }
+    
+    // FORMULARIO (ALTA)
+    @GetMapping("/nuevo")
+    public String mostrarFormularioAlta(Model modelo) {
+        Huesped nuevoHuesped = new Huesped();
+        nuevoHuesped.setId(0);
+        
+        modelo.addAttribute("huesped", nuevoHuesped);
+        
+    /*  java.util.Map<String, String> usuarioFalso = new java.util.HashMap<>();
+        usuarioFalso.put("rol", "recepcionista");
+        modelo.addAttribute("usuarioSesion", usuarioFalso); */
+        
+        return "Formulariohuesped";
+    }
+    
+    // FORMULARIO (EDICIÓN)
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEdicion(@PathVariable("id") int id, Model modelo) {
+        Huesped hExistente = huespedServicio.obtenerPorId(id);
+        modelo.addAttribute("huesped", hExistente);
+        
+    /*  java.util.Map<String, String> usuarioFalso = new java.util.HashMap<>();
+        usuarioFalso.put("rol", "recepcionista");
+        modelo.addAttribute("usuarioSesion", usuarioFalso); */
+        
+        return "Formulariohuesped";
+    }
+    
+     // PROCESAR FORMULARIO
+    @PostMapping("/guardar")
+    public String guardarHuesped(@ModelAttribute("huesped") Huesped elHuesped) {
+        huespedServicio.guardar(elHuesped);
+        return "redirect:/huespedes";
+    }
+
+    // ELIMINAR
+    @GetMapping("/eliminar/{id}")
+    public String eliminarHuesped(@PathVariable("id") int id) {
+        huespedServicio.eliminar(id);
+        return "redirect:/huespedes";
+    }
+}
