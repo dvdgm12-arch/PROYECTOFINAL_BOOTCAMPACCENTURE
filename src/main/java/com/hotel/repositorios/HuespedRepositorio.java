9a170891644fe107861ab9a2548d353673e01fff
dvdgm12-arch <dvdgm12@gmail.com>
2026-05-11 20:17:32 +0200

    MERGE A DEVELOP completado

---
+++ src/main/java/com/hotel/repositorios/HuespedRepositorio.java
@@ -0,0 +1,15 @@
+package com.hotel.repositorios;
+
+import java.util.List;
+import com.hotel.entidades.Huesped;
+
+public interface HuespedRepositorio {
+    
+    List<Huesped> obtenerTodas();
+    
+    Huesped obtenerPorId(Integer id);
+    
+    void guardar(Huesped huesped);
+    
+    void eliminar(Integer id);
+}
---
+++ src/main/java/com/hotel/servicios/impl/HuespedServicioImpl.java
@@ -0,0 +1,52 @@
+package com.hotel.servicios.impl;
+
+	
+	import java.util.List;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Service;
+	import org.springframework.transaction.annotation.Transactional;
+
+
+	import com.hotel.entidades.Huesped;
+	import com.hotel.repositorios.HuespedRepositorio;
+	import com.hotel.servicios.HuespedServicio;
+
+	@Service
+	public class HuespedServicioImpl implements HuespedServicio {
+
+	    @Autowired
+	    private HuespedRepositorio huespedRepositorio;
+
+	    @Override
+	    @Transactional
+	    public List<Huesped> obtenerTodas() {
+	    	return huespedRepositorio.obtenerTodas();
+	    	
+	    }
+	    
+	    @Override
+	    @Transactional
+	    public Huesped obtenerPorId(Integer id) {
+	    	return huespedRepositorio.obtenerPorId(id);
+	       
+	    }
+
+	    @Override
+	    @Transactional
+	    public void guardar(Huesped huesped) {
+	    	 huespedRepositorio.guardar(huesped);
+	    
+	    }
+
+	    @Override
+	    @Transactional
+	    public void eliminar(Integer id) {
+	    	huespedRepositorio.eliminar(id);
+	      
+	    }
+	    
+}
+
+
+
