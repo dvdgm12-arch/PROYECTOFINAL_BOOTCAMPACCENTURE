865817dde5df5f3b4d50a3fd3ccb8e474ac52e51
dvdgm12-arch <dvdgm12@gmail.com>
2026-05-12 16:54:32 +0200

    FASE 3 A: CARGA ARCHIVOS COMUNES DAO SERVICIO Y SERVICIOIMPL

---
+++ src/main/java/com/hotel/servicios/impl/ReservaServicioImpl.java
@@ -0,0 +1,41 @@
+package com.hotel.servicios.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hotel.entidades.Reserva;
+import com.hotel.repositorios.ReservaRepositorio;
+import com.hotel.servicios.ReservaServicio;
+
+@Service
+public class ReservaServicioImpl implements ReservaServicio {
+
+    @Autowired
+    private ReservaRepositorio reservaRepositorio;
+
+    @Override
+    @Transactional
+    public List<Reserva> obtenerTodos() {
+        return reservaRepositorio.obtenerTodos();
+    }
+
+    @Override
+    @Transactional
+    public Reserva obtenerPorId(Integer id) {
+        return reservaRepositorio.obtenerPorId(id);
+    }
+
+    @Override
+    @Transactional
+    public void guardar(Reserva reserva) {
+        reservaRepositorio.guardar(reserva);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Integer id) {
+        reservaRepositorio.eliminar(id);
+    }
+}
